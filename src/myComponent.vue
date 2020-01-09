968c160ebb1e3988bcb6d48446b79f48481534bf
hanz520 <569691266@qq.com>
2020-01-09 23:59:29 +0800

    简化目录编写组件

---
+++ src/myComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>显示传进来的文字{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'myComponent',
+  props: {
+    msg: {
+      type: String,
+      default: '默认值'
+    }
+  }
+
+}
+</script>
